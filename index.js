dd71cca965b5be3c9230cc64dfeda99003caad6a
Ramtin <31255934+rradfar@users.noreply.github.com>
2020-12-01 15:54:46 +0400

    Add passportJS

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth02").Strategy;
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
